e51346d18470ee7e249c219a2e94f0113d5cd625
harshit-bhatt7 <106058390+harshit-bhatt7@users.noreply.github.com>
2022-06-15 19:39:24 +0530

    Add files via upload
    swapping program

---
+++ swapping variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{int a=10,b=20;
+printf("Value of a is %d\n",a);
+printf("Value of b is %d\n",b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("Value of a is %d\n",a);
+printf("Value of b is %d\n",b);}
