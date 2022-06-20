eef9ac35140336093a4d0e21751d348f56c980f0
harshit-bhatt7 <106058390+harshit-bhatt7@users.noreply.github.com>
2022-06-20 10:19:21 +0530

    Add files via upload
    ..

---
+++ Mobile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{int m;
+printf("Enter your budget\n",m);
+scanf("%d",&m);
+if(m==100000-85000)
+{printf("Apple\n");}
+else if(m==84000-68000)
+{printf("Google pixal\n");}
+else if(m==67000-45000)
+{printf("Samsung\n");}
+else if(m==44000-30000)
+{printf("1 PLUS\n");}
+else if(m==29000-15000)
+{printf("Realme\n");}
+else if(m<15000)
+{printf("Oppo/Vivo\n");}
+printf("Thank you");}
