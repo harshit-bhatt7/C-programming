3ed8e2869cb02a63b2a736273a66025f72776979
harshit-bhatt7 <106058390+harshit-bhatt7@users.noreply.github.com>
2022-07-24 09:38:56 +0530

    Add files via upload

---
+++ isdigit 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+void main()
+{
+    char c;
+
+    printf("Enter a character: ");
+    scanf("%c",&c);
+
+    if (isdigit(c) == 0)
+     {
+         printf("%c is not a digit.",c);
+     }
+    else
+    {
+         printf("%c is a digit.",c);
+    }
+}
